2f94e50c34cfa6feb83b4b7466eb6f7e759ac214
Pexe <davidhenriqusms18@gmail.com>
2026-01-30 23:59:30 -0400

    Ajusta tipagem de correlationId e status

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import "./types/express";
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
